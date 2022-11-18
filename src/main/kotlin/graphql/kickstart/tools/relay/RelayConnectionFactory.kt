b364a4d88c8199db39ace379671636f85fe7f3dd
guodage <guodage003@gmail.com>
2022-11-18 15:15:43 +0800

    6.0.2.OnePiece.5 add cursorResetDuration in PageInfo.

--- src/main/kotlin/graphql/kickstart/tools/relay/RelayConnectionFactory.kt
+++ src/main/kotlin/graphql/kickstart/tools/relay/RelayConnectionFactory.kt
@@ -58,6 +58,7 @@ class RelayConnectionFactory : TypeDefinitionFactory {
     private fun createPageInfo(): ObjectTypeDefinition =
             ObjectTypeDefinition.newObjectTypeDefinition()
                     .name("PageInfo")
+                    .fieldDefinition(FieldDefinition("cursorResetDuration", NonNullType(TypeName("Int"))))
                     .fieldDefinition(FieldDefinition("totalCount", NonNullType(TypeName("Int"))))
                     .fieldDefinition(FieldDefinition("hasPreviousPage", NonNullType(TypeName("Boolean"))))
                     .fieldDefinition(FieldDefinition("hasNextPage", NonNullType(TypeName("Boolean"))))
